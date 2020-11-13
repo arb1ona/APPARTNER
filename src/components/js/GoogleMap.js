20fe77a5c537d1a897be85928108ab64ab4c387d
arb1ona <arbiona.shahu@gmail.com>
2020-11-13 11:01:30 +0100

    Show Modal

--- src/components/js/GoogleMap.js
+++ src/components/js/GoogleMap.js
@@ -1,14 +1,24 @@
 /*global google*/
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 import mapStyles from "./mapStyles";
-
+import { ButtonCheckMore } from "../ButtonCheckMore";
+const MapCard = (picture, onClick) => {
+  return (
+    <div>
+      <img style={{ width: "13rem", height: "10rem" }} src={picture} />
+      <br />
+      <ButtonCheckMore onClick={onClick} />
+    </div>
+  );
+};
 class GoogleMap extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      markers: []
-    }
+      markers: [],
+      isModalShown: false,
+    };
   }
   //use componentWillReceiveProps instead of componentDidUpdate
   componentWillReceiveProps(nextProps) {
@@ -29,11 +39,22 @@ class GoogleMap extends React.Component {
 
   hideAll() {
     const { markers } = this.state;
-    markers.forEach(marker => {
+    markers.forEach((marker) => {
       marker.infoWindow.close();
-    })
+    });
   }
-
+  // close() {
+  //   const { markers } = this.state;
+  //   markers.forEach(marker => {
+  //     marker.infoWindow.close();
+  //   })
+  // }
+  handleModalOpen() {
+    this.setState({ isModalShown: true });
+  }
+  handleModalOpen() {
+    this.setState({ isModalShown: false });
+  }
   componentDidMount() {
     const { properties, activeProperty, options } = this.props;
     const { latitude, longitude } = activeProperty;
@@ -43,25 +64,21 @@ class GoogleMap extends React.Component {
         disableDefaultUI: true,
         zoomControl: true,
         styles: mapStyles,
-
       },
       center: { lat: latitude, lng: longitude },
       mapTypeControl: true,
-      zoom: 14
+      zoom: 14,
     });
 
     this.createMarkers(properties);
   }
-
-
-
 
   createMarkers(properties) {
     const { markers } = this.state;
     const { setActiveProperty, activeProperty } = this.props;
     const activePropertyIndex = activeProperty.index;
 
-    properties.map(property => {
+    properties.map((property) => {
       const { address, index, latitude, longitude, city, picture } = property;
       this.marker = new google.maps.Marker({
         position: { lat: latitude, lng: longitude },
@@ -71,42 +88,45 @@ class GoogleMap extends React.Component {
         //text: `${index + 1}`,
         // },
         icon: {
-          url: 'https://svgshare.com/i/QBj.svg',
+          url: "https://svgshare.com/i/QBj.svg",
           scaledSize: new google.maps.Size(50, 40),
           origin: new google.maps.Point(0, 0),
           //anchor: new google.maps.Point(0, 30),
         },
-        animation: google.maps.Animation.DROP
+        animation: google.maps.Animation.DROP,
       });
 
       const infoWindow = new google.maps.InfoWindow({
-        content: `<div><img style='width: 13rem; height:10rem' src=${picture}></div>`
-        // content: `<div style='float:left'><img src=${picture}></div><div style='float:right; padding: 10px;'><b>Title</b><br/>${address}<br/>${city}</div>`
-      })
+        content: <MapCard picture={picture} onClick={this.handleModalOpen} />,
+        // content: `<div style='float:left'><img src=${picture}></div><div style='float:right; padding: 10px;'><b>Title</b><br />${address}<br />${city}</div>`
+      });
 
       this.marker.infoWindow = infoWindow;
 
-      this.marker.addListener('click', function () {
-        //hide all other info windows on click
-        this.hideAll()
-        // set active property ono the state
-        setActiveProperty(property, true);
-      }.bind(this)); // important to bind this
+      this.marker.addListener(
+        "click",
+        function () {
+          //hide all other info windows on click
+          this.hideAll();
+          // set active property ono the state
+          setActiveProperty(property, true);
+        }.bind(this)
+      ); // important to bind this
 
       // push this marker to the markers array on the state
       markers.push(this.marker);
 
       //show active property info window
       // this.showInfoWindow(activePropertyIndex);
-    })
+    });
   }
 
   render() {
     return (
       <div className="mapContainer">
-        <div id="map" ref={(el) => this.map = el}></div>
+        <div id="map" ref={(el) => (this.map = el)}></div>
       </div>
-    )
+    );
   }
 }
 
@@ -114,6 +134,6 @@ GoogleMap.propTypes = {
   properties: PropTypes.array.isRequired,
   activeProperty: PropTypes.object.isRequired,
   setActiveProperty: PropTypes.func.isRequired,
-}
+};
 
 export default GoogleMap;
