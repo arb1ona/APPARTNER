8a1c728518b4618a4bd9b90678cb9e9572a3bd95
arb1ona <arbiona.shahu@gmail.com>
2020-11-18 23:52:54 +0100

    HOuse Details

--- src/components/js/House.js
+++ src/components/js/House.js
@@ -1,244 +1,84 @@
 import React from 'react';
-import Card from './Card';
-import Header from './Header';
-import GoogleMap from './GoogleMap';
 import data from './data/Data';
-import jump from 'jump.js';
-import { easeInOutCubic } from './utils/Easing'
+import Colors from './details/Colors'
+import './Details.css';
+import DetailsThumb from './details/DetailsThumb';
 
 class House extends React.Component {
 
-  constructor(props) {
-    super(props);
+  state = {
+    products: [
+      {
+        "_id": "593e9297e17df20c4a237d42",
+        "title": "Nike Shoes",
+        "src": [
+            "https://www.upsieutoc.com/images/2020/06/27/img1.jpg",
+            "https://www.upsieutoc.com/images/2020/06/27/img2.jpg",
+            "https://www.upsieutoc.com/images/2020/06/27/img3.jpg",
+            "https://www.upsieutoc.com/images/2020/06/27/img4.jpg"
+          ],
+        "description": "UI/UX designing, html css tutorials",
+        "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+        "price": 23,
+        "colors":["red","black","crimson","teal"],
+        "count": 1
+      }
+    ],
+    index: 0
+  };
 
-    this.state = {
-      properties: data.properties,
-      activeProperty: data.properties[10],
-      filterIsVisible: false,
-      filterBedrooms: 'any',
-      filteredProperties: [],
-      isFiltering: false
-    }
+  myRef = React.createRef();
+  //constructor(props) {
+    // super(props);
+    // this.myRef = React.createRef();
 
-  //   this.setActiveProperty = this.setActiveProperty.bind(this);
-  //   this.toggleFilter = this.toggleFilter.bind(this);
-  //   this.handleFilterChange = this.handleFilterChange.bind(this);
-  //   this.filterProperties = this.filterProperties.bind(this);
+    // this.state = {
+    //   properties: data.properties,
+    //   activeProperty: data.properties[10]
+    // }
+   //}
 
-   }
-
-   
-   // handleFilterChange(e) {
-  //   const target = e.target;
-  //   const { value, name } = target;
-  //   //console.log(`${value} ${name}`);
-  //   this.setState({
-  //     [name]: value
-  //   }, function () {
-  //     this.filterProperties();
-  //   })
-
-  // }
-  
-  // filterProperties() {
-  //   const { properties, filterBedrooms } = this.state;
-  //   const isFiltering = filterBedrooms !== 'any';
-  //   console.log(isFiltering + filterBedrooms);
-
-  //   this.setState({
-  //     filteredProperties: ['some data'],
-  //     isFiltering
-  //   })
-  // }
-  
-  // toggleFilter(e) {
-    //   e.preventDefault();
-    //   this.setState({
-      //     filterIsVisible: !this.state.filterIsVisible
-      //   })
-      // }
-      
-      setActiveProperty(property, scroll) {
-        const { index } = property;
-        
-        this.setState({
-          activeProperty: property
-        })
-        
-        // only scroll if we click on the pin, not the card
-        if (scroll) {
-          // scroll to the right property
-          const target = `#card-${index}`;
-          jump(target, {
-            duration: 800,
-        easing: easeInOutCubic
-      })
-    }
-
-  }
+   handleTab = index =>{
+     this.setState({index: index})
+     const images = this.myRef.current.children;
+     for(let i=0; i<images.length; i++){
+       images[i].className = images[i].className.replace("active", "");
+     }
+     images[index].className = "active";
+   };
 
   componentDidMount() {
-    const id = new URLSearchParams(document.location.search).get("id");
-    console.log(id)
-    
+    const {index} = this.state;
+   this.myRef.current.children[index].className = "active";
   }
-
-
+  
+  
   render() {
-    const { properties, activeProperty, filterIsVisible } = this.state;
-    // console.log(params.get("id"))
+    const id = new URLSearchParams(document.location.search).get("id");
+    const { properties, index } = this.state;
+    
     return (
+    <div className="app">
+      {properties.filter(house => house._id.includes(id) ).map(filteredHouse => (
+        
+        <div className="details" key={filteredHouse._id}>
+          <div className="big-img">
+          <img src={filteredHouse.picture} alt=""/>
+          </div>        
+          <div className="box">
+                <div className="row">
+                  <h2>{filteredHouse.title}</h2>
+                  <span>${filteredHouse.price}</span>
+                </div>
+          </div>
+          <DetailsThumb images={filteredHouse.src} tab={this.handleTab} myRef={this.myRef} /> 
 
-      <div>
-         <div className="new__filters"></div>
-          {
-            (property => {
-              return <Card
-                key={property._id}
-                property={property}
-                activeProperty={activeProperty}
-                setActiveProperty={this.setActiveProperty}
-              />
-            })
-          }
-<h1>it works</h1>
-        {/* <GoogleMap
-          properties={properties}
-          activeProperty={activeProperty}
-          setActiveProperty={this.setActiveProperty}
-        /> */}
-      </div>
+          <button className="cart">Add to cart</button>
+        </div>
+      ))}
+    </div>
     )
   }
 }
 
 export default House;
-
-// import React, { Component } from 'react';
-// import Card from './Card';
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-// import GoogleMap from './GoogleMap';
-// import Header from './Header';
-// import data from './data/Data';
-// import jump from 'jump.js'
-// import { easeInOutCubic } from 'ez.js';
-
-
-// class ListingScreen extends Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       properties: data.properties,
-//       activeProperty: data.properties[0]
-//     }
-
-//     this.setActiveProperty = this.setActiveProperty.bind(this);
-//   }
-
-//   setActiveProperty(property, scroll) {
-//     const { index } = property;
-//     this.setState({
-//       activeProperty: property,
-//     });
-
-//     //only scroll if we click on the pin, not the card
-//     if (scroll) {
-//       // scroll to the right property
-//       const target = `#card-${index}`;
-//       jump(target, {
-//         duration: 2000,
-//         // offset: 0,
-//         // callback: undefined,
-//         easing: easeInOutCubic
-//       });
-//     }
-//   }
-
-//   render() {
-//     const { properties, activeProperty } = this.state
-//     return (
-//       <div>
-//         <GoogleMap
-//           activeProperty={activeProperty}
-//           properties={properties}
-//           setActiveProperty={this.setActiveProperty}
-//         />
-
-//         <div className="cards container">
-//           <div className="cards-list row ">
-//             <Header />
-//             {
-//               properties.map(property => {
-//                 return <Card
-//                   activeProperty={activeProperty}
-//                   key={property._id}
-//                   property={property}
-//                   setActiveProperty={this.setActiveProperty}
-//                 />
-//               })
-//             }
-//           </div>
-//         </div>
-
-//         {/* <GoogleMap
-//           activeProperty={activeProperty}
-//           properties={properties}
-//           setActiveProperty={this.setActiveProperty} /> */}
-//       </div>
-
-
-//     )
-//   }
-// }
-
-// export default ListingScreen;
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import '../../App.css';
-// import PropTypes from 'prop-types';
-
-
-// const House = ({ property, activeProperty, setActiveProperty }) => {
-//   const { address, bathrooms, bedrooms, carSpaces, city, index, picture, price, name } = property;
-
-//   return (
-//   <div>
-//   <h1 className='products'>House Details</h1>
-//   <div id={`card-${index}`} className={`card col-sm-12 col-md-6 ${property === activeProperty ? 'is-active' : ''}`} onClick={() => setActiveProperty(property, false)}>
-//       <img src={picture} alt={city} />
-
-//       <h5><b>{name}</b></h5>
-//       <div className="details">
-//         {/* <span className="index">{index + 1}</span> */}
-//         {/* <p className="location">
-//           {address},{city}
-//         </p> */}
-//         <ul className="features">
-//           <li className="icon-bed">{bedrooms}<span>bedrooms</span></li>
-//           <li className="icon-bath">{bathrooms}<span>bathrooms</span></li>
-//           <li className="icon-car">{carSpaces}<span>parking spots</span></li>
-//           <span className="price">ALL {price}<span>/month</span></span>
-//         </ul>
-//       </div>
-//     </div>
-//     </div>
-//   ) 
-// }
-
-
-// House.propTypes = {
-//   property: PropTypes.object.isRequired,
-//   activeProperty: PropTypes.object.isRequired,
-//   setActiveProperty: PropTypes.func
-// }
-
-// export default House;
